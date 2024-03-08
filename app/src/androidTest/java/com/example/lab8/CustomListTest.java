bd1eee4fa67ebb4bcfdad66b181b442648797f55
“Abdullah <abdullah.faisal.karim@gmail.com>
2024-03-08 13:36:36 -0700

    addCityTest

---
+++ app/src/androidTest/java/com/example/lab8/CustomListTest.java
@@ -0,0 +1,26 @@
+package com.example.lab8;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+
+public class CustomListTest {
+
+    private CustomList list;
+
+
+    public CustomList MockCityList(){
+        list = new CustomList(null,new ArrayList<>());
+        return list;
+    }
+
+    @Test
+    public void addCityTest(){
+        list = MockCityList();
+        int listSize = list.getCount();
+        list.addCity(new City("Estevan", "SK"));
+        assertEquals(list.getCount(),listSize + 1);
+    }
+}
